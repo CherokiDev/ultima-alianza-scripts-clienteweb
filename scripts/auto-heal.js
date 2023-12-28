aca0c0dbd6349074d033d13b0d218d34b07118a4
CherokiDev <miguelangelmorato84@gmail.com>
2023-12-29 00:18:43 +0100

    script auto-heal v0.2

--- scripts/auto-heal.js
+++ scripts/auto-heal.js
@@ -3,24 +3,25 @@
 Script: Auto Heal
 Author: CHEROKI
 Server: Ultima Alianza
-Current Version: 0.1
-Description: El script se encarga de curar al personaje cuando este se encuentra herido
+Current Version: 0.2
+Description: El script se encarga de curar al personaje cuando este se encuentra herido, ademas de avisar de la vida que tiene tras curarse y limpiar las vendas sucias.
 
 */
 
 // Configuracion
-const dirtyBandages = 0xE20;
-const waterContainer = 0x1008;
+const dirtyBandagesType = 0xE20;
+const waterContainerType = 0x1008;
 
-const version = 0.1
+// Informacion
+const version = 0.2
 
 client.sysMsg('Auto Heal by CHEROKI', 33)
 client.sysMsg(`Version ${version}`, 66)
 
-
+// Codigo
 let canApplyBandages = true;
-journal.clear()
 
+journal.clear()
 
 function healPlayer() {
   if (player.hits < player.maxHits && canApplyBandages) {
@@ -37,7 +38,7 @@ function healPlayer() {
     canApplyBandages = false
     journal.clear()
   }
-  
+
   if (journal.waitForText("Ya puedes usar las vendas", "System") && !canApplyBandages) {
     client.headMsg(`VENDAZO, VIDA ${player.hits}`, player, 66)
     canApplyBandages = true;
@@ -45,9 +46,11 @@ function healPlayer() {
 }
 
 function clearBandages() {
-  if (dirtyBandages && dirtyBandages.amount > 10) {
-    player.useType(dirtyBandages.graphic, undefined, player.backpack)
-    target.wait()
+  const dirtyBandages = client.findType(dirtyBandagesType)
+  const waterContainer = client.findType(waterContainerType)
+  if (dirtyBandages) {
+    player.use(dirtyBandages)
+    target.wait(1000)
     target.entity(waterContainer)
   }
 }
@@ -58,14 +61,45 @@ while (true) {
   sleep(100)
 }
 
+
+//////////////////////////////////////////////
+
 /* 
+
 CHANGELOG
 ---------
 2023-12-28
 Version 0.1
 
 - Primera version del script
-- Cura al personaje cuando este se encuentra herido y avisa de la vida que tiene tras curarse
+- Cura al personaje cuando este se encuentra herido y avisa de la vida que tienes tras curarse
 ---------
 
+2023-12-29
+Version 0.2
+
+- Se agrega la funcion de limpiar las vendas sucias
+
 */
+
+//////////////////////////////////////////////
+
+/*
+
+TODO
+----
+- (DONE) Agregar la funcion de limpiar las vendas sucias
+
+*/
+
+//////////////////////////////////////////////
+
+/*
+
+TO FIX
+------
+- Amontonar la venda que se limpia con las que ya estan limpias
+
+*/
+
+//////////////////////////////////////////////
