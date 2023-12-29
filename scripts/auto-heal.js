9f4b2e028faf420b04842f94aefe3ee0db1a9425
CherokiDev <miguelangelmorato84@gmail.com>
2023-12-29 19:05:51 +0100

    script auto-heal v0.2.2

--- scripts/auto-heal.js
+++ scripts/auto-heal.js
@@ -3,23 +3,24 @@
 Script: Auto Heal
 Author: CHEROKI
 Server: Ultima Alianza
-Current Version: 0.2.1
+Current Version: 0.2.2
 Description: El script se encarga de curar al personaje cuando este se encuentra herido, ademas de avisar de la vida que tiene tras curarse y limpiar las vendas sucias.
 
 */
 
 // Configuracion
-const dirtyBandagesType = 0xE20;
-const waterContainerType = 0x1008;
+const quantityDirtyBandages = 1 // numero de vendas sucias apiladas antes de ser limpiadas
 
 // Informacion
-const version = "0.2.1"
+const version = "0.2.2"
 
 client.sysMsg('Auto Heal by CHEROKI', 33)
 client.sysMsg(`Version ${version}`, 66)
 
 // Codigo
 let canApplyBandages = true;
+const dirtyBandagesType = 0xE20;
+const waterContainerType = 0x1008;
 
 journal.clear()
 
@@ -48,9 +49,10 @@ function healPlayer() {
 function clearBandages() {
   const dirtyBandages = client.findType(dirtyBandagesType)
   const waterContainer = client.findType(waterContainerType)
-  if (dirtyBandages) {
-    player.use(dirtyBandages)
-    target.wait(500)
+
+  if (dirtyBandages && dirtyBandages.amount > quantityDirtyBandages ) {
+    player.useType(dirtyBandagesType, undefined, player.backpack)
+    target.wait()
     target.entity(waterContainer)
   }
 }
@@ -64,7 +66,7 @@ while (true) {
 
 //////////////////////////////////////////////
 
-/* 
+/*
 
 CHANGELOG
 ---------
@@ -85,6 +87,13 @@ Version 0.2
 Version 0.2.1
 
 - Reducido el tiempo de espera para limpiar las vendas sucias a 500ms
+---------
+
+2023-12-29
+Version 0.2.2
+
+- Fixeado el bug que hacia que el personaje no limpiara las vendas sucias
+- Agregada la opcion de configurar la cantidad de vendas sucias apiladas antes de ser limpiadas
 
 */
 
@@ -95,6 +104,7 @@ Version 0.2.1
 TODO
 ----
 - (DONE) Agregar la funcion de limpiar las vendas sucias
+- Amontonar la venda que se limpia con las que ya estan limpias
 
 */
 
@@ -104,7 +114,7 @@ TODO
 
 TO FIX
 ------
-- Amontonar la venda que se limpia con las que ya estan limpias
+-
 
 */
 
