e980c7b6fb83565fab26d8a782a95876f03c7a13
CherokiDev <miguelangelmorato84@gmail.com>
2024-07-06 18:16:46 +0200

    macro manual reactive armor

---
+++ scripts/m08_reactive-armor.js
@@ -0,0 +1,48 @@
+/* 
+
+Author: CHEROKI
+Server: Ultima Alianza
+Current Version: 0.1
+Description: 
+
+*/
+
+// Codigo
+player.cast(Spells.ReactiveArmor);
+
+//////////////////////////////////////////////
+
+/* 
+
+CHANGELOG
+---------
+2024/07/06
+Version 0.1
+
+- Primera version del script
+- 
+---------
+
+*/
+
+//////////////////////////////////////////////
+
+/*
+
+TODO
+----
+- 
+
+*/
+
+//////////////////////////////////////////////
+
+/*
+
+TO FIX
+------
+- 
+
+*/
+
+//////////////////////////////////////////////
