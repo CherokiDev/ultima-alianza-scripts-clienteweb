9f5ec867a7dec338bdf1d221737a8e5ebd84ff89
CherokiDev <miguelangelmorato84@gmail.com>
2024-07-06 17:52:43 +0200

    macro manual in mani y in vas mani

---
+++ scripts/m05_in-vas-mani.js
@@ -0,0 +1,50 @@
+/* 
+
+Author: CHEROKI
+Server: Ultima Alianza
+Current Version: 0.1
+Description: 
+
+*/
+
+// Codigo
+player.cast(Spells.GreaterHeal);
+target.wait();
+target.self();
+
+//////////////////////////////////////////////
+
+/* 
+
+CHANGELOG
+---------
+2024/07/06
+Version 0.1
+
+- Primera version del script
+- 
+---------
+
+*/
+
+//////////////////////////////////////////////
+
+/*
+
+TODO
+----
+- 
+
+*/
+
+//////////////////////////////////////////////
+
+/*
+
+TO FIX
+------
+- 
+
+*/
+
+//////////////////////////////////////////////
